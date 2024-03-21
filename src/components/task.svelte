4c733276ba8458c451a7485b4e158fe12370178c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-21 18:16:51 -0400

    added check functionality; added conditional styles for checked items

--- src/components/task.svelte
+++ src/components/task.svelte
@@ -1,15 +1,17 @@
 <script lang='ts'>
     import type { Task } from "../types/types"
-    import {deleteTask} from "../stores/taskStore"
+    import {deleteTask, changeStatus} from "../stores/taskStore"
     export let taskDetails:Task;
 </script>
 
 <div>
-    <input type="checkbox" />
-    <p>{taskDetails.task}</p>
+    <input type="checkbox" on:change={() => changeStatus(taskDetails)} />
+    <p class:checked={taskDetails.status}>{taskDetails.task}</p>
     <button on:click={() => deleteTask(taskDetails.task)}>Delete</button>
 </div>
 
 <style>
-
+    .checked {
+        text-decoration: line-through;
+    }
 </style>
