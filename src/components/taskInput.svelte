33c597c77ddd9ec5be15394c4f0bc16c3421cd7e
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-21 18:58:30 -0400

    added error handling for empty tasks

--- src/components/taskInput.svelte
+++ src/components/taskInput.svelte
@@ -1,17 +1,31 @@
 <script lang="ts">
     import {addTask, deleteTask, removeCompletedTasks} from '../stores/taskStore'
     $: newTask =''
+    let error: string = ''
 
     const handleNewTask = () => {
-        addTask(newTask)
+        if (!checkForErrors()) {
+            addTask(newTask)
+        }
         newTask = ''
+    }
+
+    const checkForErrors = () => {
+        if (newTask === '') {
+            error = 'Task cannot be empty'
+            return true
+        }
+        error = ''
     }
 </script>
 
 <div>
-    <input type="text" placeholder="Add a task" bind:value={newTask}/>
-    <button on:click={handleNewTask}>Add Task</button>
-    <button on:click={removeCompletedTasks}>Remove Completed Tasks</button>
+    <div>
+        <input type="text" placeholder="Add a task" bind:value={newTask}/>
+        <button on:click={handleNewTask}>Add Task</button>
+        <button on:click={removeCompletedTasks}>Remove Completed Tasks</button>
+    </div>
+    <p>{error}</p>
 </div>
 
 <style>
