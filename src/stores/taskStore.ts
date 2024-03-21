966f64e98a1d1f5b8103d9886057f2254d90115f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-21 17:27:40 -0400

    created addTask function; added exports

--- src/stores/taskStore.ts
+++ src/stores/taskStore.ts
@@ -4,3 +4,13 @@ import type { TaskList } from "../types/types";
 const initial: TaskList[] = []
 
 const TaskStore = writable(initial);
+
+
+const addTask = (newTask: string) => {
+    TaskStore.update(tasks => {
+        return tasks = [...tasks, {task: newTask, status: false}]
+    })
+}
+
+
+export {TaskStore, addTask}
