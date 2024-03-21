1b4f1549fdb5e89210229535419b1f3591790fa2
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-21 17:56:17 -0400

    added conditional rendering for task list

--- src/components/taskList.svelte
+++ src/components/taskList.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
     import { TaskStore } from "../stores/taskStore";
-    import task from './task.svelte'
+    import Task from './task.svelte'
 </script>
 
 <div>
-
+    {#if $TaskStore.length === 0}
+        <p>There are no tasks to do</p>
+    {:else}
+        {#each $TaskStore as taskDetails (taskDetails.task)}
+            <Task {taskDetails} />
+        {/each}
+    {/if}
 </div>
 
 <style>
