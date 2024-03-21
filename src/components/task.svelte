a8ab4c423a08be2d46e6ace48fc9a474a88f3c1f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-21 18:02:59 -0400

    created basic task layout and functionality

--- src/components/task.svelte
+++ src/components/task.svelte
@@ -1,10 +1,13 @@
 <script lang='ts'>
     import type { Task } from "../types/types"
+    import {deleteTask} from "../stores/taskStore"
     export let taskDetails:Task;
 </script>
 
 <div>
-
+    <input type="checkbox" />
+    <p>{taskDetails.task}</p>
+    <button on:click={() => deleteTask(taskDetails.task)}>Delete</button>
 </div>
 
 <style>
