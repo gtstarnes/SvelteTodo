1b25639e58b30a214be626f3c2452f96dc2a60cc
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-21 18:32:38 -0400

    added function to reset newTask to an empty string after a task has been added

--- src/components/taskInput.svelte
+++ src/components/taskInput.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
     import {addTask, deleteTask, removeCompletedTasks} from '../stores/taskStore'
     $: newTask =''
+
+    const handleNewTask = () => {
+        addTask(newTask)
+        newTask = ''
+    }
 </script>
 
 <div>
     <input type="text" placeholder="Add a task" bind:value={newTask}/>
-    <button on:click={() => addTask(newTask)}>Add Task</button>
+    <button on:click={handleNewTask}>Add Task</button>
     <button on:click={removeCompletedTasks}>Remove Completed Tasks</button>
 </div>
 
