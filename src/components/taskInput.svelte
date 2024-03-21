e02e32c28f3ede9906ad6e6b25c93e1da75b9c29
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-21 18:26:25 -0400

    added button to remove all completed tasks

--- src/components/taskInput.svelte
+++ src/components/taskInput.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-    import {addTask, deleteTask} from '../stores/taskStore'
+    import {addTask, deleteTask, removeCompletedTasks} from '../stores/taskStore'
     $: newTask =''
 </script>
 
 <div>
     <input type="text" placeholder="Add a task" bind:value={newTask}/>
     <button on:click={() => addTask(newTask)}>Add Task</button>
+    <button on:click={removeCompletedTasks}>Remove Completed Tasks</button>
 </div>
 
 <style>
