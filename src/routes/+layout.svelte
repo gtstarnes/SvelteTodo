0f7f49497975b0c34bb842999d95dcacd5e885e6
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-18 21:02:52 -0500

    added header to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import Footer from "../components/footer.svelte";
+	import Header from "../components/header.svelte";
 
 
 </script>
 
 <main>
+    <Header />
     <slot></slot>
     <Footer />
 </main>
