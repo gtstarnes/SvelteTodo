7203b247dcbd8f8de91748b088081f08f6026bc1
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-18 21:01:37 -0500

    created basic header

---
+++ src/components/header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<header>
+    <div>svelteTODO</div>
+    <div>LOG OUT</div>
+</header>
+
+<style>
+    header {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
